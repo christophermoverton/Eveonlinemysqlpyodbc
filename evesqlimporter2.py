a83677c176a539c62b34aaaf80f024cb692169d4
Christopher Overton <christophermoverton@gmail.com>
2019-08-31 16:36:15 -0500

    save

--- evesqlimporter2.py
+++ evesqlimporter2.py
@@ -501,7 +501,7 @@ class Evesqlimporter:
         self.executesqllist(sqllinelist, self.cursor)
 ##        self.win.destroy()
 ##renamefilestobackup()
-cnxn = pyodbc.connect('DRIVER={MySQL ODBC 3.51 Driver};SERVER=localhost;DATABASE=testdb;UID=root;PWD=youpassword')
+cnxn = pyodbc.connect('DRIVER={MySQL ODBC 3.51 Driver};SERVER=localhost;DATABASE=testdb;UID=root;PWD=****')
 cursor = cnxn.cursor()
 eimporter = Evesqlimporter(INISETTINGS,cursor,cnxn)
 eimporter.run()
